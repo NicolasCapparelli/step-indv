62587473fed61a0dbea2cbc3465ead564fbb154f
Nico <capparellinicog123@hotmail.com>
2020-05-26 18:47:07 -0400

    Setup Vue Implement Home page Implement NavBar component Create Resume page

---
+++ portfolio/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <v-app>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons" rel="stylesheet">
+        <NavBar/>
+        
+        <v-content>
+            <router-view :key="$route.fullPath"/>
+        </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+    @import url(https://fonts.googleapis.com/css?family=Roboto&display=swap);
+
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      -ms-overflow-style: none;
+    }
+
+    * ::-webkit-scrollbar {
+        display: none;
+    }
+
+    body {
+        font-family: 'Roboto', sans-serif;
+        line-height: 1.4;
+    }
+
+    #app {
+        background-color: #292929;
+        min-height: 100vh;
+    }
+</style>
---
+++ portfolio/src/components/NavBar.vue
@@ -0,0 +1,110 @@
+<template>
+    <div id="nav-container"> 
+        <v-app-bar id="toolbar" app color="#343333" elevation="2">
+            <v-list-item two-line>
+                <v-list-item-avatar>
+                        <v-img src="https://cdn.vuetifyjs.com/images/lists/3.jpg"></v-img>
+                </v-list-item-avatar>
+                <v-list-item-content>                
+                    <v-list-item-title style="font-weight: bold;">Nicolas Capparelli</v-list-item-title>
+                    <v-list-item-subtitle>Software Engineer</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+            <v-spacer></v-spacer>
+            <v-app-bar-nav-icon v-on:click="drawer = !drawer"></v-app-bar-nav-icon>
+        </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      right
+    >
+      <v-list-item>
+            <v-list-item-avatar>
+                <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+                <v-list-item-title>Nicolas Capparelli</v-list-item-title>
+            </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list>
+            <v-list-item 
+                v-for="page in pageList"
+                :key="page.name"
+                v-on:click="navigateTo(page.name)"
+                link
+            >
+                <v-list-item-icon>
+                    <v-icon>{{page.icon}}</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                    <v-list-item-title>{{page.name}}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+
+        </v-list>
+    </v-navigation-drawer>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Template',
+    props: {
+    msg: String
+    },
+
+    methods: {
+        navigateTo(route) {
+            this.$router.push({
+                name: route
+            })
+        }
+    },
+
+    data () {
+        return {
+            drawer: false,
+            pageList: [
+                {
+                    name: "Home",
+                    icon: "mdi-home"
+                },
+
+                {
+                    name: "Resume",
+                    icon: "mdi-file"
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+    #nav-container {
+        width: 100%; 
+        height: auto;
+    }
+
+    #toolbar {
+        width: 100%;
+    }
+
+    @media screen and (min-width: 1200px){
+        #toolbar {
+            padding: 0 20%;
+        }
+    }
+
+</style>
