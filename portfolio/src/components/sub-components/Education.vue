370221116b5736f7d9d702d33325d365528ad451
Nico <capparellinicog123@hotmail.com>
2020-05-28 14:55:32 -0400

    - Decouple sections of resume to their own components

---
+++ portfolio/src/components/sub-components/Education.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="education-container">
+        <h2 class="section-title">EDUCATION</h2>
+
+        <div class="education-box">
+            <v-avatar>
+                <img
+                    src="https://cdn.vuetifyjs.com/images/john.jpg"
+                    alt="Kennesaw State University"
+                >
+            </v-avatar>            
+            <div>
+                <h1 style="font-size: 18px">Kennesaw State University</h1>                        
+                <span style="font-size: 16px;">BS Computer Science, May 2022</span><br/>
+                <span style="font-size: 14px; color: gray;">3.5 GPA</span>
+
+                <ul style="margin: 1rem 0;">
+                    <li>Programming Principles I &amp; II</li>
+                    <li>Intro to Software Engineering</li>
+                    <li>Intro to Database Programming</li>
+                    <li>Computer Org &amp; Arch</li>
+                    <li>Discrete Math</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Education',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #education-container {
+        width: 100%;
+        height: auto;        
+    }
+
+    .section-title {
+        font-size: 20px;
+        font-weight: normal;
+        margin: 1rem 0;
+        color: #919191;
+    }
+
+    .education-box {
+        width: 100%;
+        height: auto;
+
+        display: grid;
+        grid-template-columns: min-content auto;
+        column-gap: 5px;
+
+        margin: 1rem 0;
+    }
+
+</style>
---
+++ portfolio/src/components/sub-components/Experience.vue
@@ -0,0 +1,114 @@
+<template>
+    <div id="experience-container">
+        
+        <h2 class="section-title">EXPERIENCE</h2>
+        
+        <div id="experience-list">
+            
+            <div 
+                class="experience-box"
+                v-for="(experience, index) in experienceList"
+                :key="experience.dates"
+            >
+                <v-avatar>
+                    <img
+                        :src="experience.image"
+                        :alt="experience.employer"
+                    >
+                </v-avatar>
+
+                <div 
+                    class="experience-box-content"                                            
+                >
+                    <h1 style="font-size: 18px">{{experience.employer}}</h1>
+                    <span style="font-size: 16px;">{{experience.title}}</span><br/>
+                    <span style="font-size: 14px; color: gray;">{{experience.dates}}</span>
+
+                    <ul style="margin: 1rem 0;">
+                        <li
+                            v-for="(bullet, index) in experience.bullets"
+                            :key="index"
+                            style="margin: 0.5rem 0;"
+                        >
+                        {{bullet}}
+                        </li>
+                    </ul>
+
+                    <v-divider v-if="index < (experienceList.length - 1)"></v-divider>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Experience',
+    props: {
+    msg: String
+    },
+    data() {
+        return {
+            experienceList: [
+              {
+                  image: "https://cdn.vuetifyjs.com/images/john.jpg",
+                  employer: "Google",
+                  title: "STEP Intern",
+                  dates: "May 2020 - August 2020",
+                  bullets: [                      
+                      "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
+                      "Quisquam quis sequi atque praesentium tempora dolore pariatur esse quidem explicabo",
+                      "Consectetur modi iste quos assumenda distinctio, tenetur cum odit, temporibus libero"
+                  ]
+              },
+
+              {
+                  image: "https://cdn.vuetifyjs.com/images/john.jpg",
+                  employer: "Stanford",
+                  title: "Volunteer Section Leader",
+                  dates: "April 2020 - May 2020",
+                  bullets: [                      
+                      "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
+                      "Quisquam quis sequi atque praesentium tempora dolore pariatur esse quidem explicabo",
+                      "Consectetur modi iste quos assumenda distinctio, tenetur cum odit, temporibus libero",
+                      "Consectetur modi iste quos assumenda distinctio, tenetur cum odit, temporibus libero"
+                  ]
+              }
+          ]       
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #experience-container {
+        width: 100%;
+        height: auto;
+    }
+
+    .section-title {
+        font-size: 20px;
+        font-weight: normal;
+        margin: 1rem 0;
+        color: #919191;
+    }
+
+    #experience-list {
+        width: 100%;
+        height: auto;
+    }
+
+    .experience-box {
+        width: 100%;
+        height: auto;
+
+        display: grid;
+        grid-template-columns: min-content auto;
+        column-gap: 5px;
+
+        margin: 1rem 0;
+    }
+
+</style>
